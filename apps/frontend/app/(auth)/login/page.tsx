7e70e8751156a718dfc1439e778a1b99d757cda9
jackpk <pawankumarbairwa879@gmail.com>
2025-03-28 22:54:06 +0530

    login and sigin background color change and add filter

--- apps/frontend/app/(auth)/login/page.tsx
+++ apps/frontend/app/(auth)/login/page.tsx
@@ -3,8 +3,8 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 const LoginPage = () => {
   return (
-    <div className=" flex items-center justify-center min-h-screen ">
-      <Card className="w-full max-w-md">
+    <div className=" flex items-center justify-center min-h-screen bg-[#f3f3f3] ">
+      <Card className="w-full max-w-md shadow-md rounded-2xl border-2 border-white bg-white/20 backdrop-blur-3xl">
         <CardHeader>
           <CardTitle className="text-center text-3xl">Login</CardTitle>
           <AuthForm type="login" />
--- apps/frontend/app/(auth)/register/page.tsx
+++ apps/frontend/app/(auth)/register/page.tsx
@@ -2,8 +2,8 @@ import AuthForm from "@/components/AuthForm";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 const RegisterPage = () => {
   return (
-    <div className=" flex items-center justify-center min-h-screen ">
-      <Card className="w-full max-w-md">
+    <div className=" flex items-center justify-center min-h-screen bg-[#f3f3f3]">
+      <Card className="w-full max-w-md shadow-md rounded-2xl border-2 border-white bg-white/20 backdrop-blur-3xl">
         <CardHeader>
           <CardTitle className="text-center text-3xl">Login</CardTitle>
           <AuthForm type="register" />
